adc8a8acf4ba48148c01a07daaceefbe9b325e9a
cgrad01 <cgrad01@gmail.com>
2017-07-24 15:39:43 -0400

    remedy two bloaters

--- tw101-exercises-master/src/main/java/com/thoughtworks/tw101/exercises/exercise8/Guess.java
+++ tw101-exercises-master/src/main/java/com/thoughtworks/tw101/exercises/exercise8/Guess.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 /**
  * Created by Duvall_Lanell on 7/4/17.
  */
+
+// Identify two dispensers, two bloaters and one OO abuser
 public class Guess {
     int guessNumber;
     ArrayList<Integer> guessAttempts = new ArrayList<Integer>();
@@ -21,13 +23,7 @@ public class Guess {
         guessAttempts.add(guess);
 
         if (guess == number) {
-            System.out.println("You guessed " + guess + ". Correct!!!!!!\n");
-            System.out.println("Your guesses were: ");
-
-            for (int num : guessAttempts) {
-                System.out.print(num + " ");
-            }
-
+            showCorrect(guess);
             return true;
         } else if (guess > number) {
             System.out.println(guess + " is too High.\n");
@@ -38,11 +34,8 @@ public class Guess {
         }
     }
 
-    public void promptRandomNumberGuessing() {
+    public void getGuess(Scanner scan) {
         int guessNum;
-        Scanner scan = new Scanner(System.in);
-
-        System.out.print("Guess the number between 1 and 100: ");
 
         try {
             guessNum = scan.nextInt();
@@ -57,5 +50,21 @@ public class Guess {
             System.out.println("Incorrect input. Expecting a number.");
         }
 
+    }
+//    extracted this method from getGuess() (bloater)
+    public Scanner promptGuess(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Guess the number between 1 and 100: ");
+        return scan;
+    }
+
+//    extracted this method from checkGuess() (bloater)
+    public void showCorrect(int guess) {
+        System.out.println("You guessed " + guess + ". Correct!!!!!!\n");
+        System.out.println("Your guesses were: ");
+
+        for (int num : guessAttempts) {
+            System.out.print(num + " ");
+        }
     }
 }
--- tw101-exercises-master/src/main/java/com/thoughtworks/tw101/exercises/exercise8/Main.java
+++ tw101-exercises-master/src/main/java/com/thoughtworks/tw101/exercises/exercise8/Main.java
@@ -9,6 +9,6 @@ public class Main {
     public static void main(String[] args) {
         Guess guessingGame = new Guess(100, 1);
 
-        guessingGame.promptRandomNumberGuessing();
+        guessingGame.getGuess(guessingGame.promptGuess());
     }
 }
